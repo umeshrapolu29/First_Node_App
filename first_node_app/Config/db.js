0f20d574bcbdc7d8d2af08ef3045fe56578dbe18
umeshrapolu29 <umeshrapolu29@gmai.com>
2026-09-13 22:00:49 +0530

    update paassword

--- first_node_app/Config/db.js
+++ first_node_app/Config/db.js
@@ -21,11 +21,11 @@ if (dbName.toLowerCase() === 'information_schema') {
 console.log(`Connecting to database: ${dbName} at host: ${dbHost}`);
 
 const pool = mysql.createPool({
-	host: dbHost,
+	host: 'bd0bexgjuwz55o2u7mkn-mysql.services.clever-cloud.com',
 	port: Number(process.env.DB_PORT || 3306),
 	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD,
-	database: dbName,
+	password: '4Q3HDKkUUcFxWL4YcJgH',
+	database: 'bd0bexgjuwz55o2u7mkn',
 	ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 	waitForConnections: true,
 	connectionLimit: 10,
